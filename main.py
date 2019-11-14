165468cdc1ee04ac2d1d0bbed74fb19099743f88
kevinam99 <kevinam99@gmail.com>
2019-11-14 20:26:56 +0530

    Created bot UI using PyQt5

--- main.py
+++ main.py
@@ -1,90 +1,104 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
-import scraper
-import webdriverdownloader as wdd
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'bot.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
 
 
-# For Firefox gecko driver: 
-gecko_dd = wdd.GeckoDriverDownloader()
-gecko_dd.download_and_install()
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-# For Chrome driver:
-# chrome_dd = wdd.ChromeDriverDownloader()
-# chrome_dd.download_and_install()
 
-# For Opera driver:
-# opera_dd = wdd.OperaChromiumDriverDownloader()
-# opera_dd.download_and_install()
+class Ui_MainWindow(object):
+    def run_bot(self):
+        print("clicked")
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(652, 415)
+        MainWindow.setFocusPolicy(QtCore.Qt.TabFocus)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../../../Downloads/presplash.xpm"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.submit_button = QtWidgets.QPushButton(self.centralwidget)
+        self.submit_button.setGeometry(QtCore.QRect(260, 270, 131, 41))
+        font = QtGui.QFont()
+        font.setFamily("Ubuntu Condensed")
+        font.setPointSize(15)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setWeight(75)
+        self.submit_button.setFont(font)
+        self.submit_button.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.submit_button.setObjectName("submit_button")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(170, 10, 331, 51))
+        font = QtGui.QFont()
+        font.setFamily("Noto Serif CJK TC")
+        font.setPointSize(24)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.username = QtWidgets.QLineEdit(self.centralwidget)
+        self.username.setGeometry(QtCore.QRect(320, 90, 171, 25))
+        self.username.setClearButtonEnabled(False)
+        self.username.setObjectName("username")
+        self.password = QtWidgets.QLineEdit(self.centralwidget)
+        self.password.setEnabled(True)
+        self.password.setGeometry(QtCore.QRect(320, 140, 171, 25))
+        font = QtGui.QFont()
+        font.setStyleStrategy(QtGui.QFont.PreferDefault)
+        self.password.setFont(font)
+        self.password.setText("")
+        self.password.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.password.setObjectName("password")
+        self.hashtags = QtWidgets.QLineEdit(self.centralwidget)
+        self.hashtags.setGeometry(QtCore.QRect(320, 190, 171, 25))
+        self.hashtags.setObjectName("hashtags")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(190, 90, 81, 17))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(190, 140, 67, 17))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(190, 190, 91, 17))
+        self.label_4.setObjectName("label_4")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
 
-class MyGrid(Widget):
-    username = ObjectProperty(None)
-    password = ObjectProperty(None)
-    hashtags = ObjectProperty(None)
-    
-    def button(self):
-        if self.username.text != '' and self.password.text != '' and len(self.hashtags.text) > 0:
-            me = scraper.InstagramBot(self.username.text, self.password.text)
-            me.login()
-            print("Logged in as " + self.username.text)
-            for hashtag in self.hashtags.text.split(','):
-                me.like_posts_in(hashtag)
-            else:
-                print("Enter all the relevant inputs. Exiting program...")
-                SystemExit()
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        MainWindow.setTabOrder(self.username, self.password)
+        MainWindow.setTabOrder(self.password, self.hashtags)
+        MainWindow.setTabOrder(self.hashtags, self.submit_button)
 
-        # me = scraper.InstagramBot(self.username.text, self.password.text, self.hashtags.text.split(','))
-        
-        
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Instagram Bot"))
+        self.submit_button.setText(_translate("MainWindow", "Submit"))
+        self.label.setText(_translate("MainWindow", "Instagram Like Bot"))
+        self.username.setPlaceholderText(_translate("MainWindow", "Username"))
+        self.password.setPlaceholderText(_translate("MainWindow", "Password"))
+        self.hashtags.setPlaceholderText(_translate("MainWindow", "Hashtags"))
+        self.label_2.setText(_translate("MainWindow", "Username"))
+        self.label_3.setText(_translate("MainWindow", "Password"))
+        self.label_4.setText(_translate("MainWindow", "Hashtags"))
+        self.submit_button.clicked.connect(self.run_bot)
 
-class IGBot(App):
-    def build(self):
-        return MyGrid()
+
 
 if __name__ == "__main__":
-    IGBot().run()
-
-# class MyGrid(GridLayout):
-#     def __init__(self, **kwargs):
-#         super(MyGrid, self).__init__(**kwargs)
-#         self.cols = 1
-
-#         self.inside = GridLayout()
-#         self.inside.cols = 2
-#         self.inside.add_widget(Label(text = "Username: "))
-#         self.usernameIn = TextInput(multiline = False)
-#         self.inside.add_widget(self.usernameIn)
-
-#         self.inside.add_widget(Label(text = "Password: "))
-#         self.passwordIn = TextInput(multiline = False)
-#         self.inside.add_widget(self.passwordIn)
-
-#         self.inside.add_widget(Label(text = "Hashtags: "))
-#         self.hashtagsIn = TextInput(multiline = False)
-#         self.inside.add_widget(self.hashtagsIn)
-
-#         self.add_widget(self.inside)
-
-#         self.submit = Button(text = "Submit", font_size = 40)
-#         self.submit.bind(on_press = self.pressed)
-#         self.add_widget(self.submit)
-
-#     def pressed(self, instance):
-#         uname = self.usernameIn.text
-#         pwd = self.passwordIn.text
-#         print(f"{uname}: {pwd}")
-#         self.usernameIn.text = ''
-#         self.passwordIn.text = ''
-        
-
-# class MyApp(App):
-#     def build(self):
-#         return MyGrid()
-#         # return Label(text = "Label1")
-    
-
-# MyApp().run()
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
